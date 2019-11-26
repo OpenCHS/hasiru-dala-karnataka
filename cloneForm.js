a857f42c54c908649bc3f81bab4d8f956c2009e9
Hiren Thacker <hithacker@gmail.com>
2019-11-26 12:10:43 +0530

    #0 | add package-lock.json and cloneForm script

---
+++ cloneForm.js
@@ -0,0 +1,45 @@
+const uuidv4 = require("uuid/v4");
+
+const form = require("./registration/registrationForm");
+const concepts = require("./registration/registrationConcepts");
+
+concepts.forEach(concept => {
+    // console.log(`test: concept ${concept.uuid}`);
+    const newUuid = uuidv4();
+    const oldUuid = concept.uuid;
+    concept.uuid = newUuid;
+    concepts.forEach(c => {
+        if(c.answers) {
+            c.answers.forEach(a => {
+               if (a.uuid === oldUuid) {
+                   a.uuid = newUuid;
+               }
+            });
+        }
+    });
+    let flag = false;
+    form.formElementGroups.forEach(feg => {
+       feg.formElements.forEach(formElement => {
+           // console.log(`test: ${formElement.uuid}`);
+           if (formElement.concept.uuid === oldUuid) {
+               flag = true;
+               // console.log(`fE.uuid: ${formElement.uuid} fEConceptUuid ${formElement.concept.uuid} oldUuid ${oldUuid}`);
+               formElement.concept.uuid = newUuid;
+           } else {
+               // console.log(`test: yolo`);
+           }
+       })
+    });
+    // console.log(`test: ${flag}`);
+});
+
+form.formElementGroups.forEach(feg => {
+    feg.uuid = uuidv4();
+    feg.formElements.forEach(fe => {
+        fe.uuid = uuidv4();
+    })
+});
+
+console.log(JSON.stringify(concepts));
+console.log("==================");
+console.log(JSON.stringify(form));
